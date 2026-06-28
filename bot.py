bab1bbad872d5c7c153a33a9e2b8e71a313f4efe
Delta Student <217562233+amitava-code@users.noreply.github.com>
2026-06-28 11:53:40 +0530

    feat: integrate LangChain agent with Gemini model for AI chatbot responses

--- bot.py
+++ bot.py
@@ -4,17 +4,31 @@ import os
 load_dotenv()
 
 import discord
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.agents import create_agent
+from langchain.messages import HumanMessage
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 client = discord.Client(intents=intents)
 
+model = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
+agent = create_agent(model=model, tools=[])
+
+
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
-    await message.channel.send("Hello!")
+    
+    content = message.content
+
+    response = agent.invoke({"messages":[HumanMessage(content)]})
+
+    agent_message = response["messages"][-1].content
+
+    await message.channel.send(agent_message)
 
 
 client.run(token=os.getenv("DISCORD_API_KEY"))
